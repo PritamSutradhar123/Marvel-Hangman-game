4f9ed8d48f46406dd046082e1d7dd912fcfc8214
Pritam Sutradhar <165550579+PritamSutradhar123@users.noreply.github.com>
2024-04-21 21:18:05 +0530

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,194 @@
+const wordList = [
+    {
+        word: "ironman",
+        hint: "Billionaire genius inventor who created a powered suit of armor to fight evil."
+    },
+    {
+        word: "captainamerica",
+        hint: "Super-soldier and leader of the Avengers, armed with a vibranium shield."
+    },
+    {
+        word: "thor",
+        hint: "Norse god of thunder and member of the Avengers, wields a mighty hammer."
+    },
+    {
+        word: "blackwidow",
+        hint: "Highly trained spy and member of the Avengers, skilled in martial arts and espionage."
+    },
+    {
+        word: "hulk",
+        hint: "Brilliant scientist who transforms into a green-skinned behemoth when enraged."
+    },
+    {
+        word: "spiderman",
+        hint: "Teenager with spider-like abilities, swings through New York City fighting crime."
+    },
+    {
+        word: "blackpanther",
+        hint: "King of Wakanda and protector of the vibranium-rich nation, skilled warrior."
+    },
+    {
+        word: "doctorstrange",
+        hint: "Master of the mystic arts, protects Earth from magical and mystical threats."
+    },
+    {
+        word: "scarletwitch",
+        hint: "Powerful witch with reality-altering abilities, member of the Avengers."
+    },
+    {
+        word: "antman",
+        hint: "Scientist who can shrink to the size of an ant and communicate with insects."
+    },
+    {
+        word: "wintersoldier",
+        hint: "Former best friend of Captain America, skilled assassin with a metal arm."
+    },
+    {
+        word: "falcon",
+        hint: "Aerial Avenger with mechanical wings, loyal ally of Captain America."
+    },
+    {
+        word: "vision",
+        hint: "Android created by Tony Stark and Bruce Banner, member of the Avengers."
+    },
+    {
+        word: "warmachine",
+        hint: "Military officer with a powerful armored suit, close friend of Iron Man."
+    },
+    {
+        word: "groot",
+        hint: "Tree-like humanoid with limited vocabulary, member of the Guardians of the Galaxy."
+    },
+    {
+        word: "rocket",
+        hint: "Genetically modified raccoon with a love for explosives and firearms."
+    },
+    {
+        word: "gamora",
+        hint: "Adopted daughter of Thanos, skilled assassin and member of the Guardians of the Galaxy."
+    },
+    {
+        word: "drax",
+        hint: "Literal-minded warrior seeking revenge against Thanos, member of the Guardians of the Galaxy."
+    },
+    {
+        word: "starlord",
+        hint: "Half-human, half-alien leader of the Guardians of the Galaxy, skilled pilot and marksman."
+    },
+    {
+        word: "nickfury",
+        hint: "Director of S.H.I.E.L.D., mastermind behind the Avengers initiative."
+    },
+    {
+        word: "loki",
+        hint: "Trickster god of mischief and adopted brother of Thor, villain turned anti-hero."
+    },
+    {
+        word: "hawkeye",
+        hint: "Skilled marksman and member of the Avengers, uses a bow and arrows."
+    },
+    {
+        word: "captainmarvel",
+        hint: "Air Force pilot turned cosmic superhero, possesses immense energy powers."
+    },
+    {
+        word: "shuri",
+        hint: "Genius inventor and princess of Wakanda, sister of Black Panther."
+    },
+    {
+        word: "okoye",
+        hint: "Wakandan warrior and general, fiercely loyal to the throne and her people."
+    },
+    {
+        word: "mantis",
+        hint: "Empathetic alien with empathic abilities, member of the Guardians of the Galaxy."
+    },
+    {
+        word: "nebula",
+        hint: "Cybernetically enhanced daughter of Thanos, seeks redemption."
+    },
+    {
+        word: "valkyrie",
+        hint: "Asgardian warrior and member of the Revengers, skilled fighter and pilot."
+    },
+    {
+        word: "wong",
+        hint: "Sorcerer and Master of the Mystic Arts, ally of Doctor Strange."
+    },
+    {
+        word: "gambit",
+        hint: "Mutant with the ability to manipulate kinetic energy, member of the X-Men."
+    },
+    {
+        word: "storm",
+        hint: "Weather-controlling mutant and former leader of the X-Men."
+    },
+    {
+        word: "cyclops",
+        hint: "Leader of the X-Men with optic blast abilities, strategic thinker."
+    },
+    {
+        word: "rogue",
+        hint: "Mutant with the ability to absorb powers and memories of others."
+    },
+    {
+        word: "magneto",
+        hint: "Powerful mutant with the ability to control magnetic fields, sometimes an adversary of the X-Men."
+    },
+    {
+        word: "jeangrey",
+        hint: "Powerful telepath and telekinetic mutant, member of the X-Men."
+    },
+    {
+        word: "deadpool",
+        hint: "Mercenary with accelerated healing abilities, known for his humor and breaking the fourth wall."
+    },
+    {
+        word: "beast",
+        hint: "Mutant with superhuman strength and agility, member of the X-Men."
+    },
+    {
+        word: "iceman",
+        hint: "Mutant with the ability to manipulate ice and cold temperatures, member of the X-Men."
+    },
+    {
+        word: "vision",
+        hint: "Android created by Ultron with a synthetic body and the Mind Stone, member of the Avengers."
+    },
+    {
+        word: "scarletwitch",
+        hint: "Powerful witch with reality-altering abilities, member of the Avengers."
+    },
+    {
+        word: "falcon",
+        hint: "Aerial Avenger with mechanical wings, loyal ally of Captain America."
+    },
+    {
+        word: "wasp",
+        hint: "Size-changing superhero with wings and blasters, member of the Avengers."
+    },
+    {
+        word: "buckybarnes",
+        hint: "Former best friend of Captain America, skilled assassin with a metal arm."
+    },
+    {
+        word: "antman",
+        hint: "Scientist who can shrink to the size of an ant and communicate with insects."
+    },
+    {
+        word: "warmachine",
+        hint: "Military officer with a powerful armored suit, close friend of Iron Man."
+    },
+    {
+        word: "nickfury",
+        hint: "Director of S.H.I.E.L.D., mastermind behind the Avengers initiative."
+    },
+    {
+        word: "quicksilver",
+        hint: "Speedster with superhuman speed, member of the Avengers and X-Men."
+    },
+    {
+        word: "agentcarter",
+        hint: "Former S.H.I.E.L.D. agent and founder of S.H.I.E.L.D.'s predecessor, strategic mind."
+    },
+];
